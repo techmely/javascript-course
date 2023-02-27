bf787f70f0928b2ec0ecc47b963424758d4ba15e
thaycacac <thaycacac@gmail.com>
2023-02-27 13:45:12 +0700

    feat: add comment in js

---
+++ 01.lam-quen-voi-javascript/07-cau-lenh-va-dau-cham-phay-trong-javascript/main.js
@@ -0,0 +1,2 @@
+console.log(1 + 2 + 3);
+[1, 2].forEach((value) => console.log(value));
---
+++ 01.lam-quen-voi-javascript/08-comment-trong-javascript/main.js
@@ -0,0 +1,9 @@
+// đây là comment 1 dòng
+console.log("hello world!"); // đây là comment 1 dòng
+
+// alert("hello world!");
+
+/* đây là comment nhiều dòng
+console.log("hello world 1!");
+console.log("hello world 2!");
+*/
