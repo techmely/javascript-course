add16c579eb442d6b5c67a6430b77c6a96901170
thaycacac <thaycacac@gmail.com>
2023-02-28 22:31:04 +0700

    feat: variable and constant

---
+++ 01.lam-quen-voi-javascript/10-bien-va-cach-dat-ten-bien-trong-javascript/main.js
@@ -0,0 +1,20 @@
+let language = "Javascript";
+
+console.log(language);
+
+language = "React";
+
+console.log(language);
+
+// Camel case
+let firstName = "Pham Ngoc";
+let lastName = "Hoa";
+// Pascal case
+
+let FirstName = "Pham Ngoc";
+let LastName = "Hoa";
+
+// Snake case
+
+let first_name = "Pham Ngoc";
+let last_name = "Hoa";
---
+++ 01.lam-quen-voi-javascript/11-hang-trong-javascript copy/main.js
@@ -0,0 +1,8 @@
+const PI = 3.14159,
+  MAX = 1000000,
+  MIN = 0;
+
+const COLOR_BLACK = '#000000'
+const COLOR_WHITE = '#ffffff'
+const COLOR_RED = '#ff0000'
+const COLOR_BLUE = '#0000ff'
