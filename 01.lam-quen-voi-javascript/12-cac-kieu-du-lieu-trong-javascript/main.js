0f60972e6fcb860570dac5580e42db1c473bbfa2
thaycacac <thaycacac@gmail.com>
2023-03-01 16:17:56 +0700

    feat: update lecture 12, 13

---
+++ 01.lam-quen-voi-javascript/12-cac-kieu-du-lieu-trong-javascript/main.js
@@ -0,0 +1,60 @@
+// Kiểu dữ liệu
+
+let x = 999;
+console.log(x)
+
+x = "hello world"
+console.log(x)
+
+// Kiểu dữ liệu boolean
+
+let isWebLoaded = true // trang web đã được tải xong
+
+console.log(isWebLoaded)
+
+let isProgramRunning = false // Chương trình đang không chạy
+
+console.log(isProgramRunning)
+
+// Kiểu dữ liệu null
+
+let language = null
+
+console.log(language)
+
+// Kiểu dữ liệu undefined
+
+let name
+
+console.log(name)
+
+// Kiểu dữ liệu number
+
+let n1 = 66; // số nguyên dương
+let n2 = -66; // số nguyên âm
+let n3 = 3.14; // số thực dương
+let n4 = -3.14; // số thực âm
+let n5 = 2e3; // => 2*10^3 = 2000
+let n6 = 2e-3; // => 2*10^(-3) = 0.002
+let n7 = 0xff; // số dạng hexa (hệ cơ số 16): 15*16 + 15 = 255
+let n8 = 067; // số dạng octa (hệ cơ số 8): 6*8 + 7 = 55
+let n9 = 0b11; // số dạng nhị phân (hệ cơ số 2): 1*2 + 1 = 3
+let n10 = Infinity
+let n11 = -Infinity
+let n12 = NaN // 0/0
+
+// Kiểu dữ liệu BigInt
+
+let reallyBigNumber = 12345678989423714982371489237894723984n
+
+// Kiểu dữ liệu string
+
+const nickname = "Thaycacac"
+const message1 = 'đây là string'
+const message2 = "đây là string"
+const message3 = `đây là ${nickname}`
+
+// Xác định kiểu dữ liệu trong javascript
+
+console.log(typeof message1)
+console.log(typeof n1)
---
+++ 01.lam-quen-voi-javascript/13-chuyen-doi-kieu-du-lieu-trong-javascript/main.js
@@ -0,0 +1,37 @@
+// Chuyển đổi kiểu dữ liệu sang string
+// console.log(String(1)); // "1"
+// console.log(String(NaN)); // "NaN"
+// console.log(String(Infinity)); // "Infinity"
+// console.log(String(true)); // "true"
+// console.log(String(false)); // "false"
+// console.log(String(null)); // "null"
+// console.log(String(undefined)); // "undefined"
+
+
+// Chuyển đổi kiểu dữ liệu sang number
+
+// console.log("10" / "2")
+// console.log("10" / 2)
+// console.log(10 / "2")
+
+// console.log(Number("")); // 0
+// console.log(Number("Hello")); // NaN
+// console.log(Number("10")); // 10
+// console.log(Number(" ")); // 0
+// console.log(Number(" 99 ")); // 99
+// console.log(Number(true)); // 1
+// console.log(Number(false)); // 0
+// console.log(Number(null)); // 0
+// console.log(Number(undefined)); // NaN
+
+// Chuyển đổi kiểu dữ liệu sang boolean
+
+console.log(Boolean(0)); // false
+console.log(Boolean(1)); // true
+console.log(Boolean(10n)); // true
+console.log(Boolean(Infinity)); // true
+console.log(Boolean("")); // false
+console.log(Boolean("hello")); // true
+console.log(Boolean("  ")); // true
+console.log(Boolean(NaN)); // false
+console.log(Boolean(undefined)); // false
